3f1722c1b78a5ae7cb7e47795ef113aec667b9f3
Kaia Smith <coniferkai@gmail.com>
2023-02-03 11:42:54 -0800

    Fixed linter

--- Fixme.py
+++ Fixme.py
@@ -3,9 +3,9 @@
 def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive.
-    ''' 
+    '''
     if n >= 0:
-        evens = [x for x in range(n+1) if x % 2 == 0]
+        evens = [x for x in range(n + 1) if x % 2 == 0]
         return evens
     else:
         return []
